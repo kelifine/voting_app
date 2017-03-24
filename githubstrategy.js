74790d29cf7befba308cc9746216f6669e3dcdb2
Keli Fine <kelfin373@gmail.com>
2017-03-24 15:42:56 +0000

    css problems

---
+++ githubstrategy.js
@@ -0,0 +1,1 @@
+var GitHubStrategy = require('passport-github2').Strategy;
---
+++ passport.js
@@ -0,0 +1,19 @@
+var GitHubStrategy = require('passport-github2').Strategy;
+
+module.exports = function(passport) {
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+
+
+
+};
