549fecbcede58feb58859fe6577c39531217178d
Keli Fine <kelfin373@gmail.com>
2017-04-18 19:51:23 +0000

    changed port

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ routes(app, passport);
 
 
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
